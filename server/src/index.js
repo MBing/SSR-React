7bcf57ae3dae18f1d7d5a14d02103a5af5bbfe1e
Martin Bing <bing.martin.photography@gmail.com>
2019-11-10 11:35:28 +0100

    feat(home): add home component

---
+++ server/src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { renderToString } = require('react-dom/server');
+const { Home } = require('../src/client/components/Home');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+  const content = renderToString(<Home />);
+
+  res.send(content);
+});
+
+app.listen(PORT, () => {
+  console.log('port listening on', PORT);
+});
