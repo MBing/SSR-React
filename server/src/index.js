3ff9bba7ce3a4b5dbaf345f33493fed8c854daa9
Martin Bing <bing.martin.photography@gmail.com>
2019-11-10 16:56:22 +0100

    feat(webpack): small updates

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const React = require('react');
 const { renderToString } = require('react-dom/server');
 const { Home } = require('../src/client/components/Home');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
   const content = renderToString(<Home />);
 
