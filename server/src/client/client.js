2941591cbaed8d966f4acf82a67aab918513f676
Martin Bing <bing.martin.photography@gmail.com>
2019-11-11 20:24:09 +0100

    feat(routes): add route separator for client and server

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Home } from './components/Home';
+import { BrowserRouter } from "react-router-dom";
+import { Routes } from "./Routes";
 
 // Regular apps use `.render`, but as this already was rendered by the server
 // you use `.hydrate` to optimize/bind all handlers to the already existing stuff
-ReactDOM.hydrate(<Home />, document.querySelector('#root'));
+ReactDOM.hydrate(
+    <BrowserRouter>
+        <Routes/>
+    </BrowserRouter>, document.querySelector('#root'));
