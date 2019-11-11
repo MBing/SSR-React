53caaef3594a1c1693c7b3bb3af7514a4387a917
Martin Bing <bing.martin.photography@gmail.com>
2019-11-11 20:02:39 +0100

    refactor(server): exclude node_modules available packages from bundle.js

--- server/webpack.server.js
+++ server/webpack.server.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base');
+const webpackNodeExternals = require('webpack-node-externals');
+
 
 const serverConfig = {
   target: 'node',
@@ -9,5 +11,7 @@ const serverConfig = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
   },
+  // Do not include (in bundle.js) packages that are available in `node_modules`
+  externals: [webpackNodeExternals()]
 };
 module.exports = merge(baseConfig, serverConfig);
