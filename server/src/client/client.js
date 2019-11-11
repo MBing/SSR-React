4d6f3949885f42d57ea1e74a252b2f5895e95dc2
Martin Bing <bing.martin.photography@gmail.com>
2019-11-11 19:56:58 +0100

    refactor(scripts): run all scripts in parallel

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -4,4 +4,4 @@ import { Home } from './components/Home';
 
 // Regular apps use `.render`, but as this already was rendered by the server
 // you use `.hydrate` to optimize/bind all handlers to the already existing stuff
-ReactDom.hydrate(<Home />, document.querySelector('#root'));
+ReactDOM.hydrate(<Home />, document.querySelector('#root'));
