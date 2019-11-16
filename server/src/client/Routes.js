244f39fbd732552eb364ca0a1e1c8d0eae89f62e
Martin Bing <bing.martin.photography@gmail.com>
2019-11-16 19:20:17 +0100

    feat(admin): add route and renderer

--- server/src/client/Routes.js
+++ server/src/client/Routes.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import App from './App';
+import AdminsListPage from './pages/AdminsListPage';
 import HomePage from './pages/HomePage';
 import NotFoundPage from './pages/NotFoundPage';
 import UsersListPage from './pages/UsersListPage';
@@ -12,6 +13,10 @@ const Routes = [
         path: '/',
         ...HomePage,
         exact: true,
+      },
+      {
+        ...AdminsListPage,
+        path: '/admins',
       },
       {
         path: '/users',
