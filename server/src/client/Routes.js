277602a1e6823899d72e9533329021a25a6cdd6c
Martin Bing <bing.martin.photography@gmail.com>
2019-11-16 17:52:16 +0100

    feat(app): add root component for rendering routes

--- server/src/client/Routes.js
+++ server/src/client/Routes.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import App from './App'
 import HomePage from './pages/HomePage';
 import UsersListPage from './pages/UsersListPage';
 
 const Routes = [
   {
-    path: '/',
-    ...HomePage,
-    exact: true,
-  },
-  {
-    path: '/users',
-    ...UsersListPage,
+    ...App,
+    routes: [
+      {
+        path: '/',
+        ...HomePage,
+        exact: true,
+      },
+      {
+        path: '/users',
+        ...UsersListPage,
+      },
+    ],
   },
 ];
 
